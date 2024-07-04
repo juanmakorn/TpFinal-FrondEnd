ff5995993fc46e0c04a85b5872d52a5a5e4b170c
Juan Manuel Gimenez <juanmauel20@gmail.com>
2024-07-04 01:39:57 -0300

    se arreglo index y cssr

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navegador from './componentes/Navegador';
 import App from './App';
-import Footer from './componentes/Footer';
+import Footer from './componentes/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
